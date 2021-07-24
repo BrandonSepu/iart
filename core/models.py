e6a2449b7ad9c2be88e146b35c9bdd2e8ea881d2
BrandonSepu <bra.sepulvedam@duocuc.cl>
2021-07-23 20:46:25 -0400

    contact html

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class usercontact(models.Model):
+    
+    name = models.CharField( max_length=50)
+    email = models.CharField( max_length=50)
+    msn = models.TextField( max_length=300)
+
+    def __str__(self):
+        return self.name
--- core/views.py
+++ core/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'web/index.html')
+
+def contact(request):
+    return render(request, 'web/contact.html')
